68fdea94d3ae4f05e108596aea6e81678c5264e1
Sam Sui <88460848+SamJSui@users.noreply.github.com>
2021-12-04 10:56:28 -0500

    v2.1
    Cleaned up code from v2.0 and added more features

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import pandas as pd
-import pickle
 import datetime as dt
 
 # Bot initialization
@@ -11,17 +10,13 @@ client = commands.Bot(command_prefix="#")
 # Loading
 load_dotenv()  # Create '.env' file and type DISCORD_TOKEN=<Discord Bot Token>
 TOKEN = os.getenv("DISCORD_TOKEN")
-users = dict({})
-df = pd.DataFrame(columns=['Date', 'ID', 'Username', 'Voice', 'Message', 'New Member'])
+df = pd.DataFrame(columns=['Date', 'ID', 'Username', 'Duration', 'Messages'])
+user_times = dict({})
 
 @client.event
 async def on_ready():
-    global users
     global df
     print("DISCORD TRACKER IS RUNNING")
-    if os.stat("users.pkl").st_size != 0:  # Load {ID: Users} Dict if the file is not empty
-        file = open("users.pkl", "rb")
-        users = pickle.load(file)
     if os.stat("data.csv").st_size != 0:
         df = pd.read_csv("data.csv")
 
@@ -29,35 +24,36 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(member, before, after):
     date = dt.datetime.today().strftime("%d-%m-%Y")
-    if not before.channel and after.channel:
-        if not ((df['Date'] == date) & (df['ID'] == member.id)).any():  # Checks if there is already an entry
-            df.loc[len(df.index)] = [date, member.id, member.name, True, 0, False]
-            df.to_csv(path_or_buf="data.csv")
+    now_date = dt.datetime.now()
+    null_time = dt.datetime(now_date.year, now_date.month, now_date.day, 0, 0)
+    if not before.channel and after.channel:  # Joins chat
+        if not ((df['Date'] == date) & (df['ID'] == member.id)).any():  # If no entry
+            df.loc[len(df.index)] = [date, member.id, member.name, null_time, 0]  # Append at the bottom of list
+            user_times.update({member.id : dt.datetime.now()})
         else:
-            row = df.index[((df['Date'] == date) & (df['ID'] == member.id)).any()]
+            row = df.index[((df['Date'] == date) & (df['ID'] == member.id)).any()]  # If entry - finds row index
             row_index = row[0][0]
-            df.at[row_index, 'Voice'] = True
-            df.to_csv(path_or_buf="data.csv")
+            df.at[row_index, 'Duration'] = null_time
+            user_times.update({member.id: dt.datetime.now()})
+    if before.channel and not after.channel:  # Leaves chat
+        row = df.index[((df['Date'] == date) & (df['ID'] == member.id)).any()]
+        row_index = row[0][0]
+        df.at[row_index, 'Duration'] += (dt.datetime.now() - user_times.get(member.id))  # Adds accumulated time
+        user_times.pop(member.id)
+        df.to_csv(path_or_buf="data.csv")
 
 @client.event
 async def on_message(message):
     date = dt.datetime.today().strftime("%d-%m-%Y")
-    user_id = str(message.author.id)
-    user_name = str(message.author.name)
-
+    time = dt.datetime.now()
     if not ((df['Date'] == date) & (df['ID'] == message.author.id)).any():
-        df.loc[len(df.index)] = [date, message.author.id, message.author.name, False, 1, False]
+        df.loc[len(df.index)] = [date, message.author.id, message.author.name, dt.datetime(1, 1, 1, 0, 0).time(), 1]
         df.to_csv(path_or_buf="data.csv")
     else:
         row = df.index[((df['Date'] == date) & (df['ID'] == message.author.id)).any()]
         row_index = row[0][0]
-        df.at[row_index, 'Message'] += 1
+        df.at[row_index, 'Messages'] += 1
         df.to_csv(path_or_buf="data.csv")
-
-@client.event
-async def on_member_join(member):
-    date = dt.datetime.today().strftime("%d-%m-%Y")
-    df.loc[len(df.index)] = [date, member.id, member.name, False, 0, True]
 
 if __name__ == "__main__":
     client.run(TOKEN)
