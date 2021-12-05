c975d6c23c63771d1c212f3b8c87b86cb3f43a84
SamJSui <samuelsui2002@gmail.com>
2021-12-05 16:53:05 -0500

    v2.1.2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Discord bot using the Discord.py API to record user data for the application's communities.
 
-## Current Version (v2.1.1)
+## Current Version (v2.1.2)
 
 - Discord Logger records and stores:
   - Dates of Activity
@@ -24,7 +24,7 @@ Discord bot using the Discord.py API to record user data for the application's c
 - This Data Science project was created to collect activity within the Discord server, allowing us to reflect on our past, in the future, in the form of data.
 
 ### Edits
-- (v2.1.1) Cleaned up
+- (v2.1.2) Edge case for post-midnight events
 - Prints to console every new entry
 - Records duration of user presence within voice channel
 - ~~Records new members in the .csv~~
--- main.py
+++ main.py
@@ -19,45 +19,61 @@ async def on_ready():
     print("DISCORD TRACKER IS RUNNING")
     if os.stat("data.csv").st_size != 0:
         df = pd.read_csv("data.csv")
+        df['Duration'] = pd.to_datetime(df['Duration'])
+        df['Date'] = pd.to_datetime(df['Date'])
 
 # Data Collection
 @client.event
 async def on_voice_state_update(member, before, after):
-    date = dt.datetime.today().strftime("%d-%m-%Y")
+
+    date = dt.datetime.today().strftime("%d-%m-%Y") # Current
     now_date = dt.datetime.now()
     null_time = dt.datetime(now_date.year, now_date.month, now_date.day, 0, 0)
+    date_tomorrow = (dt.datetime.today() + dt.timedelta(days=1)).strftime("%d-%m-%Y") # Tomorrow
+    null_tomorrow = null_time + dt.timedelta(days=1)
+
     if not before.channel and after.channel:  # Joins chat
-        if not ((df['Date'] == date) & (df['ID'] == member.id)).any():  # If no entry
-            df.loc[len(df.index)] = [date, member.id, member.name, null_time, 0]  # Append at the bottom of list
+        if not ((df['Date'] == date) & (df['ID'] == member.id)).any(): # If no entry
+            df.loc[len(df.index)] = [date, member.id, member.name, null_time, 0]
+            df.loc[len(df.index)] = [date_tomorrow, member.id, member.name, null_tomorrow, 0] # Append at the bottom of list
             user_times.update({member.id : dt.datetime.now()})
         else:
-            row = df.index[((df['Date'] == date) & (df['ID'] == member.id)).any()]  # If entry - finds row index
-            row_index = row[0][0]
+            row = df.index[((df['Date'] == date) & (df['ID'] == member.id))].tolist()
+            row_index = row[0]
             if df.at[row_index, 'Duration'] != null_time:
                 user_times.update({member.id: dt.datetime.now()})
+            if not ((df['Date'] == date_tomorrow) & (df['ID'] == member.id)).any():
+                df.loc[len(df.index)] = [date_tomorrow, member.id, member.name, null_tomorrow, 0]
     if before.channel and not after.channel:  # Leaves chat
         row = df.index[((df['Date'] == date) & (df['ID'] == member.id))].tolist()
         row_index = row[0]
         df.at[row_index, 'Duration'] += (dt.datetime.now() - user_times.get(member.id))  # Adds accumulated time
         user_times.pop(member.id)
-        df.to_csv(path_or_buf="data.csv")
-        print(df.tail())
+        df.sort_values(by=['Date'], ascending=False)
+        df.to_csv(path_or_buf="data.csv", index=False)
+        print(df.loc[row_index])
 
 @client.event
 async def on_message(message):
+
     date = dt.datetime.today().strftime("%d-%m-%Y")
+    date_tomorrow = (dt.datetime.today() + dt.timedelta(days=1)).strftime("%d-%m-%Y")
     now_date = dt.datetime.now()
     null_time = now_date.replace(hour=0, minute=0, second=0, microsecond=1)
+    null_tomorrow = null_time + dt.timedelta(days=1)
+
     if not ((df['Date'] == date) & (df['ID'] == message.author.id)).any():
         df.loc[len(df.index)] = [date, message.author.id, message.author.name, null_time, 1]
-        df.to_csv(path_or_buf="data.csv")
-        print(df.tail())
+        df.loc[len(df.index)] = [date_tomorrow, message.author.id, message.author.name, null_tomorrow, 1]
+        df.sort_values(by=['Date'], inplace=True, ascending=False)
+        df.to_csv(path_or_buf="data.csv", index=False)
     else:
         row = df.index[((df['Date'] == date) & (df['ID'] == message.author.id))].tolist()
         row_index = row[0]
         df.at[row_index, 'Messages'] += 1
-        df.to_csv(path_or_buf="data.csv")
-        print(df.tail())
+        df.sort_values(by=['Date'], inplace=True, ascending=False)
+        df.to_csv(path_or_buf="data.csv", index=False)
+        print(df.loc[row_index])
 
 if __name__ == "__main__":
     client.run(TOKEN)
